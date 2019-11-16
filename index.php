a6323b27bc66623117593c53fec98882392044e9
Stelmaszv <stelmaszv@gmail.com>
2019-11-16 14:38:14 +0100

    added templete

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ use core\content\CTemplate;
 use core\content\sectionGenarate;
 use core\validate\validate;
 use core\count\mainCount;
-$template=new CTemplate('index.htm');
-$template->CAdd('[#header#]',sectionGenarate::section('header.htm'));
-$template->CAdd('[#footer#]',sectionGenarate::section('footer.htm'));
+$template=new CTemplate('templete/index.htm');
+$template->CAdd('[#header#]',sectionGenarate::section('templete/header.htm'));
+$template->CAdd('[#footer#]',sectionGenarate::section('templete/footer.htm'));
 $valdate=new validate();
 if($valdate->validate($_POST)){
     $result=new mainCount($_POST);
