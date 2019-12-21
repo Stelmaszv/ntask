25d5059a24c4ec0559c36b76ce35e5002aafc775
Stelmaszv <stelmaszv@gmail.com>
2019-12-21 18:42:43 +0100

    upadete typevalid

--- core/validate/validate.php
+++ core/validate/validate.php
@@ -3,14 +3,13 @@ namespace core\validate;
 use core\validate\colorvalid;
 use core\validate\rectanglevalid;
 use core\validate\varvalid;
-use core\validate\typevalid;
 use core\validate\observervalidator;
 class validate{
     public function validate($data){
         if(isset($data['count'])){
             $validator=new observervalidator();
             $validator->add(new colorvalid($data,'color'));
-            $validator->add(new typevalid($data,'geometricshapetype'));
+            $validator->add(new varvalid($data,'geometricshapetype'));
             $validator->add(new varvalid($data,'var1'));
             $validator->add(new rectanglevalid($data,'var2'));
             $validator->validateStart();
